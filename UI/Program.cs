27f30828eca2700ed05f32f2ee84b00df1c75d08
gangothri.a <gangothri.a@GSPS2P9S542>
2025-01-06 19:57:20 +0530

    Add project files.

---
+++ UI/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
